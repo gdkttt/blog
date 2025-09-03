ef5c09fa656c1de2c1f8a1921aaea21cce923eca
gdkttt <411452723@qq.com>
2025-09-03 20:36:17 +0800

    delete

--- dist/assets/astro.BUlK1JWx.js
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server.CnfVVFb5.js';
-import 'clsx';
--- dist/assets/consts.CfrzebMq.js
+++
@@ -1,4 +0,0 @@
-const SITE_TITLE = "退场修行";
-const SITE_DESCRIPTION = "不要担心未来，不要后悔过去。";
-
-export { SITE_DESCRIPTION as S, SITE_TITLE as a };
